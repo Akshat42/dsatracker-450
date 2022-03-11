1239de4cb35ed93ff1002f24296e41d84bdf5b92
Akshat42 <akshatdivya03@gmail.com>
2022-03-11 17:15:18 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>450 DSA Tracker</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import GlobalProgress from "../../components/GlobalProgress/GlobalProgress";
+
+function Home() {
+    return (
+        <>
+            <GlobalProgress />
+        </>
+    );
+}
+
+export default Home;
