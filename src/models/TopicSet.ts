ffe1d3a50391dccde1223d188dac97d2770bbdd5
Akshat42 <akshatdivya03@gmail.com>
2022-03-21 19:19:07 +0530

    add data models

---
+++ src/models/TopicSet.ts
@@ -0,0 +1,16 @@
+export interface TopicSet {
+    topicName: string,
+    started: boolean,
+    position: number,
+    doneQuestions: number,
+    questions: Question[]
+};
+
+export interface Question {
+    Bookmark: boolean,
+    Done: boolean,
+    Notes: string,
+    Problem: string,
+    Topic: string,
+    URL: string
+};
